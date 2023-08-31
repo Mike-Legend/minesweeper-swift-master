1df21f2684d51a741595f5a155ef29481c62ac84
Mike-Legend <michael.george.why@gmail.com>
2023-08-31 10:57:31 -0700

    Initial Commit

---
+++ Minesweeper/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Minesweeper
+//
+//  Created by Alain Gilbert on 3/10/18.
+//  Copyright © 2018 None. All rights reserved.
+//
+// Setting up to reformat - Mike
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
